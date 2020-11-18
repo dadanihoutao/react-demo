db2dcce2ef11edc2272c1dd140d8e6759210d9e6
ayang <851051279@qq.com>
2020-11-18 11:07:33 +0800

    整理代码格式

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route,BrowserRouter,Link,Switch } from "react-router-dom"
+import { Route, BrowserRouter, Link, Switch } from "react-router-dom"
 import LoadableComponent from './loadable'
 
 const HomeIndex = LoadableComponent(() => import(/* webpackChunkName: 'homePage' */ '@/views/home/Home'))
